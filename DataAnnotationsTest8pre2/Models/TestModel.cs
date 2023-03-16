c7ba1b1f35c60129d2ea17cc6899cf20b428ab50
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-03-16 22:50:05 +0900

    change

---
+++ DataAnnotationsTest8pre2/Models/TestModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationsTest8pre2.Models
+{
+    public class TestModel
+    {
+        [Required(DisallowAllDefaultValues = true)]
+        public DateOnly Col1 { get; set; } //= DateOnly.FromDateTime(DateTime.Now);
+        [Range(0, 10, MinimumIsExclusive = false, MaximumIsExclusive = false)]
+        public int Col2 { get; set; }
+        [Range(0, 10, MinimumIsExclusive = true, MaximumIsExclusive = true)]
+        public int Col3 { get; set; }
+        [Length(8,20)]
+        public string Col4 { get; set; } = "password";
+        [AllowedValues("Microsoft","Apple","Amazon","Facebook","Google")]
+        public string Col5 { get; set; } = "Microsoft";
+        [DeniedValues("Excel", "Word")]
+        public string Col6 { get; set; } = "PowerPoint";
+    }
+}
